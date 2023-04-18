13b8b817131f321982fa9384b20159ee297f827a
Albina Peposhi <codegirl.al@gmail.com>
2023-04-18 22:35:04 +0200

    norm errors fix

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 22:24:23 by apeposhi          #+#    #+#             */
-/*   Updated: 2023/04/18 22:24:24 by apeposhi         ###   ########.fr       */
+/*   Updated: 2023/04/18 22:34:53 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,39 +14,39 @@
 
 static void send_binary_message(char *message, int pid)
 {
-    if (!message)
-        exit(EXIT_FAILURE);
+	if (!message)
+		exit(EXIT_FAILURE);
 
-    for (int i = 0; message[i]; i++)
-    {
-        char letter = message[i];
-        for (int j = 7; j >= 0; j--)
-        {
-            int bit = (letter >> j) & 1;
-            if (bit == 1)
-            {
-                if (kill(pid, SIGUSR1) == -1)
-                    exit(1);
-            }
-            else
-            {
-                if (kill(pid, SIGUSR2) == -1)
-                    exit(1);
-            }
-            usleep(100);
-        }
-    }
+	for (int i = 0; message[i]; i++)
+	{
+		char letter = message[i];
+		for (int j = 7; j >= 0; j--)
+		{
+			int bit = (letter >> j) & 1;
+			if (bit == 1)
+			{
+				if (kill(pid, SIGUSR1) == -1)
+					exit(1);
+			}
+			else
+			{
+				if (kill(pid, SIGUSR2) == -1)
+					exit(1);
+			}
+			usleep(100);
+		}
+	}
 }
 
 int main(int argc, char *argv[])
 {
-    int pid;
+	int pid;
 
-    if (argc != 3)
-    {
-        ft_printf("Invalid arguments have been entered.\n");
-        exit(EXIT_FAILURE);
-    }
-    pid = ft_atoi(argv[1]);
-    send_binary_message(argv[2], pid);   
+	if (argc != 3)
+	{
+		ft_printf("Invalid arguments have been entered.\n");
+		exit(EXIT_FAILURE);
+	}
+	pid = ft_atoi(argv[1]);
+	send_binary_message(argv[2], pid);   
 }
