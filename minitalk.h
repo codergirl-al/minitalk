6a7e948dbc0d8b56c348473692a63e1c5278cea2
Albina Peposhi <codegirl.al@gmail.com>
2023-04-18 22:25:07 +0200

    Added 42 header

--- minitalk.h
+++ minitalk.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/18 22:23:59 by apeposhi          #+#    #+#             */
+/*   Updated: 2023/04/18 22:24:00 by apeposhi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINITALK_H
 # define MINITALK_H
 
@@ -5,7 +17,7 @@
 #include <signal.h>
 
 //util functions
-char    ft_convert_binary(char *binary);
-int     ft_validate_client_string(const char **str);
+char	ft_convert_binary(char *binary);
+int		ft_validate_client_string(const char **str);
 
 #endif
