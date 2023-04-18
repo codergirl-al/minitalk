30163573519d2c080e4584017fb74c93e25a594b
Albina Peposhi <codegirl.al@gmail.com>
2023-04-18 23:43:19 +0200

    norm errors fix

--- client.c
+++ client.c
@@ -6,41 +6,47 @@
 /*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 22:24:23 by apeposhi          #+#    #+#             */
-/*   Updated: 2023/04/18 22:34:53 by apeposhi         ###   ########.fr       */
+/*   Updated: 2023/04/18 23:40:44 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static void send_binary_message(char *message, int pid)
+static void	check_signal(int pid, int signal)
 {
+	if (kill(pid, signal) == -1)
+		exit(1);
+}
+
+static void	send_binary_message(char *message, int pid)
+{
+	char	letter;
+	int		bit;
+	int		i;
+	int		j;
+
 	if (!message)
 		exit(EXIT_FAILURE);
-
-	for (int i = 0; message[i]; i++)
+	i = -1;
+	j = 8;
+	while (message[++i])
 	{
-		char letter = message[i];
-		for (int j = 7; j >= 0; j--)
+		letter = message[i];
+		while (j-- >= 0)
 		{
-			int bit = (letter >> j) & 1;
+			bit = (letter >> j) & 1;
 			if (bit == 1)
-			{
-				if (kill(pid, SIGUSR1) == -1)
-					exit(1);
-			}
+				check_signal(pid, SIGUSR1);
 			else
-			{
-				if (kill(pid, SIGUSR2) == -1)
-					exit(1);
-			}
+				check_signal(pid, SIGUSR1);
 			usleep(100);
 		}
 	}
 }
 
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-	int pid;
+	int	pid;
 
 	if (argc != 3)
 	{
@@ -48,5 +54,5 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 	pid = ft_atoi(argv[1]);
-	send_binary_message(argv[2], pid);   
+	send_binary_message(argv[2], pid);
 }
