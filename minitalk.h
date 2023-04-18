aedc211fd1eb5ae7ef881cbb616b8fb2e70481fa
Albina Peposhi <codegirl.al@gmail.com>
2023-04-18 22:26:42 +0200

    norm errors fix

--- minitalk.h
+++ minitalk.h
@@ -6,15 +6,15 @@
 /*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 22:23:59 by apeposhi          #+#    #+#             */
-/*   Updated: 2023/04/18 22:24:00 by apeposhi         ###   ########.fr       */
+/*   Updated: 2023/04/18 22:26:22 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
-#include "libft/includes/libft.h"
-#include <signal.h>
+# include "libft/includes/libft.h"
+# include <signal.h>
 
 //util functions
 char	ft_convert_binary(char *binary);
