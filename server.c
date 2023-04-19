850603c9b57d4be33157e807bcd6b392b8b13063
Albina Peposhi <apeposhi@3-B-8.42heilbronn.de>
2023-04-19 16:36:24 +0200

    Adding changes

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 22:24:10 by apeposhi          #+#    #+#             */
-/*   Updated: 2023/04/18 23:43:05 by apeposhi         ###   ########.fr       */
+/*   Updated: 2023/04/19 16:35:26 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,17 @@
 
 void	sig_handler(int sig_num, siginfo_t *sig_info, void *context)
 {
-	static char	received_char;
-	static int	bit_count;
-	char		c;
+	static char	received_char = 0;
+	static int	bit_count = 0;
+	static char	c = 0;
 
 	if (sig_num == SIGUSR1)
 		received_char |= (1 << bit_count);
 	else if (sig_num == SIGUSR2)
-		received_char &= -(1 << bit_count);
+    	received_char &= (1 << bit_count);
+	ft_printf("wiiwiwi %s", received_char);
 	bit_count++;
-	if (bit_count == 8)
+	if (bit_count == 7)
 	{
 		c = ft_convert_binary(&received_char);
 		if (c == '\0')
@@ -42,13 +43,18 @@ int	main(void)
 {
 	struct sigaction	sa;
 
-	sa.sa_sigaction = &sig_handler;
-	sa.sa_flags = SA_SIGINFO;
-	ft_printf("PID: %d\n", getpid());
+	if (ft_printf("PID = %d\n", getpid()) == -1)
+        return (1);
+    sa.sa_sigaction = sig_handler;
+    sa.sa_flags = SA_SIGINFO;
+    if (sigaction(SIGUSR2, &sa, NULL) != 0
+        || sigaction(SIGUSR1, &sa, NULL) != 0)
+    {
+        write(2, "sigaction error\n", 16);
+        return (1);
+    }
 	while (1)
 	{
-		sigaction(SIGUSR1, &sa, NULL);
-		sigaction(SIGUSR2, &sa, NULL);
 		pause();
 	}
 	return (0);
