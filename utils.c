3b734ba2816cfdf0163106be2df830b281df22fb
Albina Peposhi <apeposhi@3-B-12.42heilbronn.de>
2023-04-21 16:18:56 +0200

    Function cleanup

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 22:24:03 by apeposhi          #+#    #+#             */
-/*   Updated: 2023/04/20 18:33:44 by apeposhi         ###   ########.fr       */
+/*   Updated: 2023/04/21 16:18:23 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,28 +89,4 @@ char	ft_convert_binary(char *binary)
 			res = res | (1 << (7 - index));
 	}
 	return (res);
-}
-
-/*
-** Check if the input pointer-to-a-string contains digits
-** If so, return 0;
-** else, continue.
-*/
-
-int	ft_validate_client_string(const char **str)
-{
-	const char	*ptr;
-
-	ptr = *str;
-	if (!str)
-		return (0);
-	if (*ptr == '-')
-		ptr++;
-	while (*ptr != '\0')
-	{
-		if (!ft_isdigit(*ptr))
-			return (0);
-		ptr++;
-	}
-	return (1);
 }
